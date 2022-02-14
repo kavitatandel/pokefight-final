f12dd3721a0f49047186a29a97c0455020633193
kavitatandel <kavita2810@gmail.com>
2022-02-14 23:26:09 +0100

    updated files

---
+++ kavita_tandel_portfolio/src/resumeData.js
@@ -0,0 +1,162 @@
+let resumeData = {
+  "imagebaseurl": "./images/Photo_Tandel_Kavitaben.jpg",
+  "name": "Kavitaben Tandel",
+  "role": "Full Stack Developer.",
+  "linkedinId": "https://www.linkedin.com/in/kavitatandel/",
+  // "skypeid": "Your skypeid",
+  "skypeid": "",
+  "roleDescription": "I am hard working, team-centric, highly driven individual with a clear set of goals and a passion for coding. I like listening music in my free time.",
+  "blockchain": "",
+  "socialLinks": [
+    {
+      "name": "linkedin",
+      "url": "https://www.linkedin.com/in/kavitatandel/",
+      "className": "fa fa-linkedin"
+    },
+    {
+      "name": "github",
+      "url": "https://github.com/kavitatandel",
+      "className": "fa fa-github"
+    },
+    // {
+    //   "name":"skype",
+    //   "url":"http://twitter.com/rbhatia46",
+    //   "className":"fa fa-twitter"
+    // }
+  ],
+  "aboutme": "I am currently doing intensive course in Web and App Development.I have knowledge in HTML5, CSS3, Javascript and MERN stack. I have around 3 years of experience as a software developer with ASP.NET, C# and MS SQL Server.",
+  "address": "Leonberg, baden württemberg, Germany",
+  // "website":"https://rbhatia46.github.io",
+  "website": "",
+  "education":
+    [
+      {
+        "UniversityName": "WBS Training",
+        "specialization": "Full-Stack App and Web Development",
+        "MonthOfPassing": "March",
+        "YearOfPassing": "2022",
+        "Achievements": "At WBS Training, I had project-based, practise oriented, remote and collaborative training.I worked on my final project Paw Pal which is a react dating app for dog and dog lovers. During my training, I worked everyweek on various projects with other team mates with agile methodology.HTML, CSS, Javascript and MERN Stack was a main focus of this training."
+      },
+      {
+        "UniversityName": "Gujarat University",
+        "specialization": "Computer Engineering",
+        "MonthOfPassing": "May",
+        "YearOfPassing": "2005",
+        "Achievements": "I had specialization in  Object Oriented Programming, Software Engineering, Database Management System, Client Server Technologies & Web Technologies during my Bachelor.I did my final semester project in Reliance Industries Limited."
+      }
+
+    ],
+  "work": [
+    {
+      "CompanyName": "WBS Training",
+      "specialization": "Full-stack Web and App development",
+      "MonthOfLeaving": "March",
+      "YearOfLeaving": "2022",
+      "Achievements": "Paw Pal is a react app. It is a dating app between dogs and their lovers. The peeple who does not have dogs and still wants to have playdate with them, they can book a playdate with dog owner and take them for a walk. This is free dating application."
+    },
+    {
+      "CompanyName": "Infosys Limited",
+      "specialization": "Software Developer",
+      "MonthOfLeaving": "May",
+      "YearOfLeaving": "2012",
+      "Achievements": "I worked on a project Pfizer-Café&T. Client was Pfizer USA. ASP.NET, C# and SQL Server 2008 were the technologies used. CAFÉ&T web portal manages Pfizer Contest activities. User created contest here, adds participants, declare winners, assigns awards, tracks award fulfilment & taxation details by using this Content Management System (CMS). "
+    },
+    {
+      "CompanyName": "KPIT Limited",
+      "specialization": "Software Developer",
+      "MonthOfLeaving": "October",
+      "YearOfLeaving": "2010",
+      "Achievements": "I developed proof of concepts for new projects."
+    },
+    {
+      "CompanyName": "Adroitus IT Services",
+      "specialization": "Software Developer",
+      "MonthOfLeaving": "May",
+      "YearOfLeaving": "2010",
+      "Achievements": "I have worked on web applications and windows application with .NET technologies and MS SQL Server.Some of the projects are taxi reservation system, telecome Infrastucture subscriber portal, logistics and warehouse management and developed business logic for managing business-critical data."
+    }
+  ],
+  "skillsDescription": "My Skills",
+  "skills": [
+    {
+      "skillname": "HTML5"
+    },
+    {
+      "skillname": "CSS3"
+    },
+    {
+      "skillname": "Javascript"
+    },
+    {
+      "skillname": "Reactjs"
+    },
+    {
+      "skillname": "Nodejs"
+    },
+    {
+      "skillname": "Express"
+    },
+    {
+      "skillname": "RESTfulAPI"
+    },
+    {
+      "skillname": "POSTGRESQL"
+    },
+    {
+      "skillname": "MongoDB"
+    },
+    {
+      "skillname": "ASPDOTNET"
+    },
+    {
+      "skillname": "CSHARP"
+    },
+    {
+      "skillname": "SQL"
+    },
+    {
+      "skillname": "MSSQLServer"
+    },
+    {
+      "skillname": "Git"
+    }
+  ],
+  "portfolio": [
+    {
+      "name": "Pokemon Fight",
+      "description": "build a react application to fetch pokemon from API and implemented feature to fight between user selected pokemon and randomly generated pokemon.",
+      "imgurl": "../images/portfolio/pokemonfight.png",
+      "url": "https://pokemonfight.vercel.app/"
+    },
+    {
+      "name": "Cook Book",
+      "description": "It is a react application to display 4 countries recipes and recipe description.",
+      "imgurl": "../images/portfolio/cookbook.png",
+      "url": "https://cookbook-beige.vercel.app/"
+    },
+    {
+      "name": "Hacker News",
+      "description": "It is a react application, a version of Hacker News with a search feature built by fetching their API",
+      "imgurl": "../images/portfolio/hackernews.png",
+      "url": ""
+    },
+    {
+      "name": "To Do List",
+      "description": "It is a small application, to create,delete,update,list and completed todo items using DOM Manipulations.",
+      "imgurl": "../images/portfolio/todo.png",
+      "url": ""
+    }
+  ],
+  "testimonials": [
+    {
+      "description": "",
+      "name": ""
+    },
+    {
+      "description": "",
+      "name": ""
+    }
+  ]
+}
+
+export default resumeData
